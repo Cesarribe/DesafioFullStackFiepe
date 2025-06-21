1be2b0fef1b7017ed6c9e3a48f0563c6b06e7864
cesarribe <cesar.ribeiro_@hotmail.com>
2025-06-21 13:24:14 -0300

    testando cores

--- frontend/src/app/components/product-list/product-list.component.ts
+++ frontend/src/app/components/product-list/product-list.component.ts
@@ -1,24 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
 @Component({
   selector: 'app-product-list',
   standalone: true,
-  imports: [CommonModule],
-  template: `
-    <h2>Produtos</h2>
-    <div class="product-grid">
-      <div *ngFor="let product of products" class="product-card">
-        <img [src]="product.imageUrl" [alt]="product.name" />
-        <h3>{{ product.name }}</h3>
-        <p>R$ {{ product.price }}</p>
-      </div>
-    </div>
-  `,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.scss']
 })
 export class ProductListComponent implements OnInit {
   products: any[] = [];
+
+  filtros = {
+    minPrice: null,
+    maxPrice: null,
+    search: ''
+  };
 
   constructor(private http: HttpClient) {}
 
@@ -26,5 +25,9 @@ export class ProductListComponent implements OnInit {
     this.http.get<any[]>('http://localhost:8080/products').subscribe(data => {
       this.products = data;
     });
+  }
+
+  aplicarFiltros(): void {
+    console.log('Filtros aplicados:', this.filtros);
   }
 }
