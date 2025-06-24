ec0b8e34ab5375e6d6f64f6bc2b5c6a1c45a1161
cesarribe <cesar.ribeiro_@hotmail.com>
2025-06-24 19:08:12 -0300

    navbar ok

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SidebarComponent } from './components/sidebar/sidebar.component'; // ajuste o caminho se necessário
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  template: `<router-outlet></router-outlet>`,
+  imports: [RouterOutlet, SidebarComponent],
+  template: `
+    <div class="layout">
+      <app-sidebar></app-sidebar>
+      <main class="conteudo">
+        <router-outlet></router-outlet>
+      </main>
+    </div>
+  `,
+  styleUrls: ['./app.component.scss'] 
 })
 export class AppComponent {}
